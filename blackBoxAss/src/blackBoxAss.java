3aab23a78ed324b5126e2ad045d20cb155e6d366
Rah-gamer <prkshrahul700@gmail.com>
2024-08-21 11:09:04 +0400

    minor changes
    Corrected the prompt messages for entering first name and last name  e.g Fistname was corrected to First Name

--- blackBoxAss/src/blackBoxAss.java
+++ blackBoxAss/src/blackBoxAss.java
@@ -39,21 +39,21 @@ public class blackBoxAss
 
         if(validateAge(Age) == true)
         {
-             System.out.print("Enter Fistname:");
+             System.out.print("Enter First name:");
             firstName = sn.nextLine();
         
         while(validateName(firstName)== false)
         {
-            System.out.print("Enter Fistname:");
+            System.out.print("Enter First name:");
             firstName = sn.nextLine();
         }
 
-        System.out.print("Enter Lastname:");
+        System.out.print("Enter Last name:");
         lastName = sn.nextLine();
         
         while(validateName(lastName)== false)
         {
-            System.out.print("Enter Lastname:");
+            System.out.print("Enter Last name:");
             lastName = sn.nextLine();
         }
 
